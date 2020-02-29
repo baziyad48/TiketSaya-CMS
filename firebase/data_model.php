eb0e7c5835333b81dd20c27db6ffea553346c8fc
baziyad48 <huhummamalybaziyad@gmail.com>
2020-02-29 18:19:39 +0700

    fix connection error.

---
+++ firebase/data_model.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'firebase.php';
+
+if (isset($_POST['sign_in'])) {
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+
+    if ($username != null) {
+        if ($password != null) {
+            //Nama child-nya nanti tolong sesuaikan.
+            $reference = 'User/' . $username;
+            $data = $database->getReference($reference)->getValue();
+
+            if ($data['username'] == $username) {
+                if ($data['password'] == $password) {
+                    session_start();
+                    $_SESSION['username'] = $username;
+                    header('location: ../dashboard.php');
+                } else {
+                    echo "Username atau Password salah!";
+                }
+            } else {
+                echo "Username atau Password salah!";
+            }
+        }
+    }
+}
